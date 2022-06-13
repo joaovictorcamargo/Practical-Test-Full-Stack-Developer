74217d5b490beb895af94f2b130d6a8fbb125f5d
joaovictor <jc7155004@gmail.com>
2022-06-13 07:47:28 -0300

    fix: minwidth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ export default function Home() {
     <Box
       pt="2.25rem"
       px="2.625rem"
+      minWidth="500px"
       w="100%"
       h="34rem"
       bgGradient="linear(to-l, pink.100, orange.100)"
